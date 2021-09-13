9d2185ae72a9bff56af62ffce6e495f7a19c2790
YauhenMurachou <emurach1992@gmail.com>
2021-09-13 20:33:21 +0300

    dont care about this changes in dev

--- src/pages/Registration/Registration.js
+++ src/pages/Registration/Registration.js
@@ -1,8 +1,8 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 import { Routes } from '../../utils/routes';
 
-import '../Registration/Registration.scss'
+import '../Registration/Registration.scss';
 
 const Registration = () => {
 	return (
@@ -11,26 +11,26 @@ const Registration = () => {
 				Page of registration
 			</h1>
 
-			<form action="#" method="post">
-				<div className="imgcontainer">
-					<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgxfDAMqK3mhyikTk6uhY8Bn3HdpjkMvuzLQ&usqp=CAU" alt="Avatar" class="avatar" />
+			<form action='#' method='post'>
+				<div className='imgcontainer'>
+					<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgxfDAMqK3mhyikTk6uhY8Bn3HdpjkMvuzLQ&usqp=CAU' alt='Avatar' class='avatar' />
 				</div>
 
-				<div className="container">
-					<label for="uname"><b>Username</b></label>
-					<input type="text" placeholder="Enter Username" name="uname" required />
-					<label for="psw"><b>Password</b></label>
-					<input type="password" placeholder="Enter Password" name="psw" required />
+				<div className='container'>
+					<label for='uname'><b>Username</b></label>
+					<input type='text' placeholder='Enter Username' name='uname' required />
+					<label for='psw'><b>Password</b></label>
+					<input type='password' placeholder='Enter Password' name='psw' required />
 
-					<button type="submit" className='sub-btn'>Login</button>
+					<button type='submit' className='sub-btn'>Login</button>
 					<label>
-						<input type="checkbox" checked="checked" name="remember" /> Remember me
+						<input type='checkbox' checked='checked' name='remember' /> Remember me
 					</label>
 				</div>
 
-				<div className="container">
-					<button type="button" className="cancelbtn">Cancel</button>
-					<span className="psw">Forgot <a href="#">password?</a></span>
+				<div className='container'>
+					<button type='button' className='cancelbtn'>Cancel</button>
+					<span className='psw'>Forgot <a href='#'>password?</a></span>
 				</div>
 			</form>
 
