eb1f487a6d4404e1caa35c5a45fb887daf4d70a2
YauhenMurachou <emurach1992@gmail.com>
2021-09-14 13:15:24 +0300

    draw form of login

--- src/pages/Registration/Registration.js
+++ src/pages/Registration/Registration.js
@@ -11,30 +11,8 @@ const Registration = () => {
 				Page of registration
 			</h1>
 
-			<form action='#' method='post' className='registr-form'>
 
-				<div className='imgcontainer'>
-					<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgxfDAMqK3mhyikTk6uhY8Bn3HdpjkMvuzLQ&usqp=CAU' alt='Avatar' class='avatar' />
-				</div>
-
-				<div className='container'>
-					<label for='uname'><b>Username</b></label>
-					<input type='text' placeholder='Enter Username' name='uname' required />
-					<label for='psw'><b>Password</b></label>
-					<input type='password' placeholder='Enter Password' name='psw' required />
-
-					<button type='submit' className='sub-btn'>Login</button>
-					<label>
-						<input type='checkbox' checked='checked' name='remember' /> Remember me
-					</label>
-				</div>
-
-				<div className='container'>
-					<button type='button' className='cancelbtn'>Cancel</button>
-					<span className='psw'>Forgot <a href='#'>password?</a></span>
-				</div>
-			</form>
-
+		
 			<button>
 				<Link to={Routes.SignInRoute} >
 					<span>Назад</span>
--- src/pages/SignIn/SignIn.js
+++ src/pages/SignIn/SignIn.js
@@ -2,15 +2,35 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Routes } from '../../utils/routes';
 
+import '../SignIn/SignIn.scss'
+
 const SignIn = () => {
 	return (
 		<div>
-			<h1>
-				Page of login
-			</h1>
-			<button>
+			<form action='#' method='post' className='registr-form'>
+
+				<div className='imgcontainer'>
+					<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgxfDAMqK3mhyikTk6uhY8Bn3HdpjkMvuzLQ&usqp=CAU' alt='Avatar' class='avatar' />
+				</div>
+
+				<div className='container'>					
+					<label for='uname'><b>Username</b></label>
+					<input type='email' placeholder='Enter Login' name='uname' className='login-input' required />
+					<label for='psw'><b>Password</b></label>
+					<input type='password' placeholder='Enter Password' name='psw' className='password-input' required />
+
+					<button type='submit' className='sub-btn'>Sign In</button>
+				</div>
+				<div className='form-or'>
+					or
+				</div>
+			</form>
+
+
+
+			<button className='registration-btn'>
 				<Link to={Routes.SignUpRoute} >
-					<span>Зарегистрироваться</span>
+					<span>Go to registration</span>
 				</Link>
 			</button>
 		</div>
