23270c95daa95b52861821528caee9d4e648b404
YauhenMurachou <emurach1992@gmail.com>
2021-09-14 17:21:11 +0300

    add checking of empty inputs

--- src/App/App.js
+++ src/App/App.js
@@ -4,10 +4,9 @@ import { BrowserRouter as Router, Route, useHistory } from "react-router-dom";
 import './App.css';
 
 import { Home, SignIn, Registration, Tasks, Users } from '../pages'
-import { Navigation, ToDoApp, ToDoList } from '../components';
+// import { Navigation, ToDoApp, ToDoList } from '../components';
 import { Routes } from '../utils/routes';
 import { AuthorizedRoute, NotAuthorizedRoute } from '../components/routes';
-
 
 const App = () => {
 
@@ -30,6 +29,7 @@ const App = () => {
 			<AuthorizedRoute exact path={Routes.TasksRoute} component={Tasks} />
 
 			<AuthorizedRoute exact path={Routes.UsersRoute} component={Users} />
+
 		</Router>
 	)
 };
--- src/pages/SignIn/SignIn.js
+++ src/pages/SignIn/SignIn.js
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Routes } from '../../utils/routes';
 
 import '../SignIn/SignIn.scss'
 
 const SignIn = () => {
+
+	const [loginForm, setLoginForm] = useState({ loginValue: '', pswValue: '' });
+	const [loginFormError, setLoginFormError] = useState({ loginError: '', pswError: '' });
+
+	const handleCheckEmptyForm = (event, inputName, errorName) => {
+		const loginFormCopy = { ...loginForm };
+		const loginFormErrorCopy = { ...loginFormError };
+
+		if (loginFormCopy[inputName] === '') {
+			loginFormErrorCopy[errorName] = 'empty'
+			setLoginFormError(loginFormErrorCopy)
+		}
+
+	}
+
+	const handleChangeLoginForm = (event, inputName, errorName) => {
+		const loginFormCopy = { ...loginForm };
+		const loginFormErrorCopy = { ...loginFormError };
+		const { value: inputValue } = event.target;
+		loginFormErrorCopy[errorName] = '';
+		setLoginFormError(loginFormErrorCopy);
+		loginFormCopy[inputName] = inputValue;
+		setLoginForm(loginFormCopy);
+	};
+
+	const { loginValue, pswValue } = loginForm;
+	const { loginError, pswError } = loginFormError;
+
+
+
 	return (
 		<div>
 			<form action='#' method='post' className='registr-form'>
@@ -13,11 +43,32 @@ const SignIn = () => {
 					<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgxfDAMqK3mhyikTk6uhY8Bn3HdpjkMvuzLQ&usqp=CAU' alt='Avatar' class='avatar' />
 				</div>
 
-				<div className='container'>					
-					<label for='uname'><b>Username</b></label>
-					<input type='email' placeholder='Enter Login' name='uname' className='login-input' required />
+				<div className='container'>
+					<label for='login'><b>Username</b></label>
+					<input type='email' placeholder='Enter Login'
+						name='login' className='login-input'
+						value={loginValue}
+						onChange={event => handleChangeLoginForm(event, 'loginValue', 'loginError')}
+						onBlur={event => handleCheckEmptyForm(event, 'loginValue', 'loginError')}
+						required />
+
+					{
+						loginError === 'empty' &&
+						<div className='login-error'>Please, enter login </div>
+					}
+
 					<label for='psw'><b>Password</b></label>
-					<input type='password' placeholder='Enter Password' name='psw' className='password-input' required />
+					<input type='password' placeholder='Enter Password'
+						name='psw' className='password-input'
+						value={pswValue}
+						onChange={event => handleChangeLoginForm(event, 'pswValue', 'pswError')}
+						onBlur={event => handleCheckEmptyForm(event, 'pswValue', 'pswError')}
+						required />
+
+					{
+						pswError === 'empty' &&
+						<div className='psw-error'>Please, enter password </div>
+					}
 
 					<button type='submit' className='sub-btn'>Sign In</button>
 				</div>
